01fb133e0f687c67b75ed20f01448c7c20ec410e
thana_pha <thana_pha@freewillsolutions.com>
2024-01-27 23:07:23 +0700

    Update vue.config.js to disable host check in devServer

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  lintOnSave: false,
-  transpileDependencies: true,
-})
+module.exports = {
+  devServer: {
+    disableHostCheck: true
+  }
+}
