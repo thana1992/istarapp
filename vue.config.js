401ec5efad820c33f1261c0bf4d58899825782e4
thana_pha <thana_pha@freewillsolutions.com>
2024-01-29 14:41:04 +0700

    Add allowedHosts configuration to devServer

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   lintOnSave: false,
   transpileDependencies: true,
+  devServer: {
+    allowedHosts: 'all',
+  }
 })
