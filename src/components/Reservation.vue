a96b207da1d6441144066e9ee77c0cabe4f79b05
thana_pha <thana_pha@freewillsolutions.com>
2024-05-02 00:21:37 +0700

    Add coursename to reservation object

--- src/components/Reservation.vue
+++ src/components/Reservation.vue
@@ -168,6 +168,7 @@ import { mapGetters } from 'vuex';
                 classday: new Date(this.date).toLocaleDateString('en-US', { weekday: 'long' }),
                 studentname: this.student.firstname + ' ' + this.student.lastname,
                 studentnickname: this.student.nickname,
+                coursename: this.student.course_shortname,
             }
             console.log('checkDuplicateReservation : ' ,reservaObj)
             const token = this.$store.getters.getToken;
