9c1365abfe8beed3283b30313277a1e0406cb724
Thana Phalodom <thana_pha@freewillsolutions.com>
2024-06-02 19:34:34 +0700

    chore: Update devServer configuration in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,10 @@ module.exports = defineConfig({
   lintOnSave: false,
   transpileDependencies: true,
   devServer: {
+    client: {
+      webSocketTransport: 'ws', // หรือ 'sockjs' หรือ 'native' ถ้าต้องการปรับแต่ง
+      webSocketURL: false, // ปิดการใช้งาน WebSocket
+    },
     allowedHosts: 'all',
     hot: false, // ปิดการใช้งาน HMR
     liveReload: false, // ปิดการใช้งานการโหลดสด
