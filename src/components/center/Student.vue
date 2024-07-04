5aa16f93cb1fce32d0df55f3195678be4cb26afd
thana_pha <thana_pha@freewillsolutions.com>
2024-07-04 17:13:30 +0700

    Refactor Student.vue to highlight expired dates in red

--- src/components/center/Student.vue
+++ src/components/center/Student.vue
@@ -152,7 +152,9 @@
                 {{ calculateAge(item.dateofbirth).int }}
             </template>
             <template v-slot:item.expiredate="{ item }">
-                {{ expireDateLeft(item.expiredate) }}
+                <p :class="{ 'highlighted-red': item.expiredate != null && expireDateLeft(item.expiredate).indexOf('หมดอายุ') > -1 }">
+                    {{ expireDateLeft(item.expiredate) }}
+                </p>
             </template>
             <template v-slot:item.edit="{ item }">
                 <v-icon size="large" color="info" @click="clickEditStudent(item)">mdi-pencil</v-icon>
@@ -235,7 +237,7 @@ export default {
     async created() {
         try {
             const token = this.$store.getters.getToken;
-            console.log("token ", token);
+            //console.log("token ", token);
             if (!token) {
                 this.errorMsg = "Not found token, Please login...";
                 this.errorDialog = true;
@@ -284,7 +286,7 @@ export default {
                 .then(({ success, results, message }) => {
                     if (success) {
                         this.StudentList = results;
-                        console.log("StudentList : ", this.StudentList);
+                        //console.log("StudentList : ", this.StudentList);
                         this.loadingStudent = false;
                     } else {
                         this.$emit("onErrorHandler", message || "Get Student list failed");
@@ -305,6 +307,10 @@ export default {
             const { valid } = await this.$refs.newstdform.validate();
             if (valid) {
                 // Make API request to register the user
+                let dob = null;
+                if(this.editedStudentItem.dateofbirth){
+                    dob = this.SQLDate(this.editedStudentItem.dateofbirth);
+                }
                 const StudentObj = {
                     studentid: this.editedStudentItem.studentid,
                     firstname: this.editedStudentItem.firstname,
@@ -312,11 +318,12 @@ export default {
                     lastname: this.editedStudentItem.lastname,
                     nickname: this.editedStudentItem.nickname,
                     gender: this.editedStudentItem.gender,
-                    dateofbirth: this.SQLDate(this.editedStudentItem.dateofbirth),
+                    dateofbirth: dob,
                     familyid: this.editedStudentItem.familyid,
                     courserefer: this.editedStudentItem.courserefer,
                     shortnote: this.editedStudentItem.shortnote,
                 };
+
                 //console.log(this.editedStudentIndex+ ' StudentObj : ', StudentObj)
                 const token = this.$store.getters.getToken;
                 if (this.editedStudentIndex > -1) {
@@ -340,7 +347,7 @@ export default {
                             }
                         })
                         .catch((error) => {
-                            console.log(error);
+                            //console.log(error);
                             if (error.response.status && error.response.status == 401) {
                                 this.$emit("onErrorHandler", error.response.data.message);
                                 this.$emit("onClickChangeState", "login");
@@ -499,7 +506,7 @@ export default {
         },
         onFileChange(e) {
             const file = e.target.files[0];
-            console.log("file : ", file);
+            //console.log("file : ", file);
 
             const maxSize = 4 * 1024 * 1024; // ขนาดสูงสุด 4MB
             if (file.size > maxSize) {
@@ -521,7 +528,7 @@ export default {
         },
         async uploadImageProfile(sid) {
             if (!this.editedStudentItem.profile_image) {
-                this.$emit("onErrorHandler", "Please select an image to upload");
+                //this.$emit("onErrorHandler", "Please select an image to upload");
                 return;
             }
             this.uploadLoading = true;
@@ -566,7 +573,7 @@ export default {
                     `/student/${this.editedStudentItem.studentid}/profile-image`,
                     { headers: { Authorization: `Bearer ${this.token}` } }
                 );
-                console.log("response : ", response);
+                //console.log("response : ", response);
                 //this.editedStudentItem.profile_image = response.data.image;
                 this.base64Image = response.data.image;
                 if (response.data.image !== null) {
@@ -595,7 +602,7 @@ export default {
                 .then((response) => {
                     //console.dir(response);
                     if (response.data.success) {
-                        console.log("getStudentUseCourse", response.data);
+                        //console.log("getStudentUseCourse", response.data);
                         const res = response.data.results;
                         if (res) {
                             const data = response.data.results[0];
@@ -614,7 +621,7 @@ export default {
                     }
                 })
                 .catch((error) => {
-                    console.log(error);
+                    //console.log(error);
                     if (error.response && error.response.status == 401) {
                         this.$emit("onErrorHandler", error.response.data.message);
                         this.$emit("onClickChangeState", "login");
@@ -673,8 +680,8 @@ export default {
             if(!expdate) return '';
             const today = new Date();
             const expirationDate = new Date(expdate);
-            console.log('today', today);
-            console.log('expirationDate', expirationDate);
+            //console.log('today', this.format_date(today, 'YYYY-MM-DD'));
+            //console.log('expirationDate', this.format_date(expirationDate, 'YYYY-MM-DD'));
 
             if (expirationDate < today) {
                 return 'หมดอายุ';
@@ -705,7 +712,7 @@ export default {
             if (days > 0) {
                 returnText += `${days} วัน`;
             }
-            console.log('returnText', returnText);
+            //console.log('returnText', returnText);
             return returnText;
             
         },
@@ -781,6 +788,10 @@ const ComponentAPI = {
     display: flex;
     justify-content: center;
     align-items: center;
+}
+
+.highlighted-red {
+  color: red;
 }
 </style>
 ```
