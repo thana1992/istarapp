1628532e16fb6a1fc9ffa5094a26ede4deb40202
thana1992 <thana.phalodom@gmail.com>
2024-12-08 17:45:29 +0700

    Update vue.config.js: Enable proxy for API requests and enhance debugging options

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,19 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 const webpack = require('webpack');
+
 module.exports = defineConfig({
   lintOnSave: false,
   transpileDependencies: true,
   devServer: {
     allowedHosts: 'all',
-    // hot: false,
-    // liveReload: false,
-    // webSocketServer: false,
+    proxy: {
+      // Proxy ทุกคำขอที่ไม่ใช่ Static Files ไปยัง SERVER_URL
+      '/': {
+        target: process.env.SERVER_URL || 'https://king-prawn-app-92x4l.ondigitalocean.app', // URL ของ API Server
+        changeOrigin: true, // จำลอง origin ให้เหมือน target
+        logLevel: 'debug', // ใช้สำหรับ Debugging (เอาออกเมื่อขึ้น Production)
+      },
+    },
   },
   configureWebpack: {
     plugins: [
@@ -18,4 +24,4 @@ module.exports = defineConfig({
       })
     ]
   }
-})
+});
